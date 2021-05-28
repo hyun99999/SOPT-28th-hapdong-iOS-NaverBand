7d95316a5fb688f05fe787dae54e88b209d96509
eunyoung <eun970923@naver.com>
2021-05-28 22:28:02 +0900

    homeview collectionview 수정

--- NaverBand/NaverBand/Sources/ViewControllers/HomeViewController.swift
+++ NaverBand/NaverBand/Sources/ViewControllers/HomeViewController.swift
@@ -4,14 +4,15 @@
 //
 //  Created by 이은영 on 2021/05/20.
 //
-
 import UIKit
-
+                                           
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var MainCollectionView: UICollectionView!
     
     private var MainList : [MainDataModel] = []
+    private var MainList2 : [MainDataModel] = []
+    private var MainList3 : [MainMoreDataModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +26,6 @@ class HomeViewController: UIViewController {
 
         let nibName2 = UINib(nibName: "MainMoreCollectionViewCell", bundle: nil)
         MainCollectionView.register(nibName2, forCellWithReuseIdentifier:"MainMoreCollectionViewCell" )
-        
-        
     }
     
 
@@ -40,13 +39,15 @@ class HomeViewController: UIViewController {
          MainDataModel(coverName: "mainImg5" , title: "킹왕짱 댄스팀",isHiddenSquare: true),
          MainDataModel(coverName: "mainImg6", title: "광희중 4-5",isHiddenSquare: true),
          MainDataModel(coverName: "mainImg7", title: "추니 여행",isHiddenSquare: true),
-         MainDataModel(coverName: "mainImg8", title: "내일로 여행",isHiddenSquare: true),
-         MainDataModel(coverName: "mainImg9", title: "바람잘날 없는 글잡이",isHiddenSquare: true),
-         MainDataModel(coverName: "mainImg10", title: "청량 얼반",isHiddenSquare: true),
-         MainDataModel(coverName: "mainImg11", title: "예쁜 옷 도매물",isHiddenSquare: true),
-         MainDataModel(coverName: "mainImg12", title: "제주도 한달 살이",isHiddenSquare: true),
-            
+         MainDataModel(coverName: "mainImg8", title: "내일로 여행",isHiddenSquare: true)
         ])
+        MainList2.append(contentsOf: [
+            MainDataModel(coverName: "mainImg9", title: "바람잘날 없는 글잡이",isHiddenSquare: false),
+            MainDataModel(coverName: "mainImg10", title: "청량 얼반",isHiddenSquare: false),
+            MainDataModel(coverName: "mainImg11", title: "예쁜 옷 도매물",isHiddenSquare: false),
+            MainDataModel(coverName: "mainImg12", title: "제주도 한달 살이",isHiddenSquare: false)
+        ])
+        MainList3.append(contentsOf: [MainMoreDataModel(label: "19개 밴드", title: "방문 뜸한 밴드 더보기")])
     }
 
 }
@@ -59,30 +60,35 @@ extension HomeViewController : UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if(section==0){
-            return 9
+            return MainList.count + MainList3.count
         }else{
-            return 4
+            return MainList2.count
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if(indexPath.section==0 && indexPath.item==8){
-            let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "MainMoreCollectionViewCell", for: indexPath) as! MainMoreCollectionViewCell
             
-            cell2.initializeData("19개 밴드", "방문 뜸한 밴드 더보기")
+        if(indexPath.section==0 && indexPath.row < 8){
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as! MainCollectionViewCell
+            cell.initializeData(image: MainList[indexPath.row].coverName, title: MainList[indexPath.row].title, isHiddenSquare: true)
+            return cell
+        }
+        else if(indexPath.section==0 && indexPath.row == 8){
+            let cell3 = collectionView.dequeueReusableCell(withReuseIdentifier: "MainMoreCollectionViewCell", for: indexPath) as! MainMoreCollectionViewCell
+            cell3.initializeData(num: MainList3[indexPath.row - 8].label, title: MainList3[indexPath.row - 8].title)
+            return cell3
             
-            return cell2
         }
         else{
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as! MainCollectionViewCell
-        
-        cell.initializeData(image :  MainList[indexPath.row].coverName, title : MainList[indexPath.row].title ,isHiddenSquare: MainList[indexPath.row].isHiddenSquare)
-        
-        return cell
+            let cell2=collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as! MainCollectionViewCell
+            cell2.initializeData(image: MainList2[indexPath.row].coverName, title: MainList2[indexPath.row].title, isHiddenSquare: false)
+            
+            return cell2
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
         var header : CollectionViewHeader?
         
         if kind == UICollectionView.elementKindSectionHeader{
@@ -125,7 +131,7 @@ extension HomeViewController : UICollectionViewDelegateFlowLayout{
         let width = UIScreen.main.bounds.width
         
         let cellWidth = width * (106/375)
-        let cellHeight = cellWidth * (120/106)
+        let cellHeight = cellWidth * (98/106)
         
         return CGSize(width: cellWidth, height: cellHeight)
     }
